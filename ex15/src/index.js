dea29758da13bf637da9883a0f506ba49655cada
arminpeco <apeco1906@gmail.com>
2021-09-20 23:25:14 +0200

    Added file index.js

---
+++ ex15/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function convertToInt(str) {
+    var a=parseInt(str,16);
+    return a;
+}
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
+// Only change code above this line
+
+module.exports=convertToInt;
