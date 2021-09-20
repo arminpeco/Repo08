477cc6fadccff145d211cb06e1895404d36e6829
arminpeco <apeco1906@gmail.com>
2021-09-20 14:21:53 +0200

    Added file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+function reusableFunction()
+{
+    //Write code below this line
+
+    console.log("Hi Padawan!");
+
+    //Write code below this line
+
+}
+reusableFunction();
+module.exports=reusableFunction;
