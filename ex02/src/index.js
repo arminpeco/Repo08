43937a243637c8ef91d9eb162b4a0153d5ef56aa
arminpeco <apeco1906@gmail.com>
2021-09-20 14:27:08 +0200

    Added file index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function functionPrintInfo(name,age)
+{
+    //Write code below this line
+
+        console.log(name,age);
+
+    //Write code below this line
+
+}
+functionPrintInfo("John",23);
+functionPrintInfo("Michael",43);
+module.exports=functionPrintInfo;
