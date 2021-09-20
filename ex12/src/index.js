db0df5b1c8e5b19fbd5d43c729b1728bd5b6080b
arminpeco <apeco1906@gmail.com>
2021-09-20 20:22:01 +0200

    Added file index.js

---
+++ ex12/src/index.js
@@ -0,0 +1,8 @@
+
+//Only change code below this line
+function randomInteger(params) {
+    return console.log(Math.floor(Math.random()*30));
+}
+//Only change code above this line
+randomInteger();
+module.exports=randomInteger;
